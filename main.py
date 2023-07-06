4ed0a6ff871c156ac4e83d5329206d98aeda2966
morgango <morgango@gmail.com>
2023-07-06 10:03:28 -0500

    Found error

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ def write_temp_file(uploaded_file):
         tmp_file_path = tmp_file.name
     return tmp_file_path
 
-def extract_text(uploaded_file, encoding="utf8", csv_loader_args={'delimiter': ','}):
+def extract_text_from_upload(uploaded_file, encoding="utf8", csv_loader_args={'delimiter': ','}):
     
     tmp_file_path = write_temp_file(uploaded_file)
-    file_type = uploaded_file.type.lower().strip():
+    file_type = uploaded_file.type.lower().strip()
     if "pdf" in file_type:
         loader = PyPDFLoader(file_path=tmp_file_path)
     elif "csv" in file_type:
@@ -58,16 +58,26 @@ def extract_text(uploaded_file, encoding="utf8", csv_loader_args={'delimiter': '
     return text
 
 
-def load_document_text(text, elasticsearch_url=None, index_name=None, embeddings=None):
+def load_document_text(text, 
+                       elasticsearch_url=None, 
+                       index_name=None, 
+                       embeddings=None, 
+                       separator="\n", 
+                       chunk_size=1000, 
+                       chunk_overlap=200, 
+                       length_function=len):
 
     # split into chunks
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
+        separator=separator,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=length_function,
     )
+
     chunks = text_splitter.split_documents(text)
+    
+    # this will upload the chunks to the elasticsearch cluster
     uploaded = ElasticVectorSearch.from_documents(chunks, embeddings,
                                                 elasticsearch_url=elasticsearch_url, 
                                                 index_name=index_name)
@@ -88,40 +98,53 @@ def load_conv_chain():
     chain = ConversationChain(llm=llm)
     return chain
 
+def get_connections(provider="openai", **kwargs):
+
+    cleaned_provider=provider.lower().strip()
+    if cleaned_provider == "openai":
+        embeddings = OpenAIEmbeddings()
+        llm = OpenAI()
+    else:
+        embeddings = OpenAIEmbeddings()
+        llm = OpenAI()
+
+    return embeddings, llm
+
+        
+
 chain = load_conv_chain()
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
 st.header("LangChain Demo")
 
+embeddings, llm = get_connections("openai")
+
 qa, dumb_chat_bot, smarter_chat_bot = st.tabs(["File Q&A - PDF,CSV,TXT", "ChatBot", "ChatBot - Smarter"])
 
 with qa:
-    st.header("Ask your PDF 💬")
+    st.header("Ask your file 💬")
     
-    # upload file
-    uploaded_files = st.file_uploader("Upload your PDF", type=['txt', 'text', 'pdf', 'csv'], accept_multiple_files=True)
-
-    # create embeddings for each individual file
-    embeddings = OpenAIEmbeddings()
-    llm = OpenAI()
+    uploaded_files = st.file_uploader("Upload your file", 
+                                      type=['txt', 'text', 'pdf', 'csv'], 
+                                      accept_multiple_files=True)
 
     for uploaded_file in uploaded_files:
 
         if uploaded_file is not None:
             
             # extract the text
-            document_text = extract_text(uploaded_file)
+            document_text = extract_text_from_upload(uploaded_file)
             
-
+            # load the data into Elasticsearch
             load_document_text(document_text, 
                                 elasticsearch_url=ELASTICSEARCH_URL, 
                                 index_name=ELASTICSEARCH_INDEX, 
                                 embeddings=embeddings)
 
-      
     # show user input
     user_question = st.text_input("Ask a question about your PDF:")
+    
     if user_question:
         answer_docs = ask_es_question(user_question, 
                             elasticsearch_url=ELASTICSEARCH_URL,
@@ -131,6 +154,7 @@ with qa:
         if answer_docs:
             chain = load_qa_chain(llm, chain_type="stuff")
             with get_openai_callback() as cb:
+                print(f"answer_docs={answer_docs}")
                 response = chain.run(input_documents=answer_docs, 
                                      question=user_question)
                 print(cb)
@@ -138,21 +162,24 @@ with qa:
             st.write(response)
 
 with dumb_chat_bot:
-   st.header("Unaugmented Chat")
+    st.header("Unaugmented Chat")
     # if "generated" not in st.session_state:
     #     st.session_state["generated"] = []
 
     # if "past" not in st.session_state:
     #     st.session_state["past"] = []
 
-
     # def get_text():
     #     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     #     return input_text
 
+
     # user_input = get_text()
 
     # if user_input:
+        ########################################################
+        # NEED to create unique chain for each tab, this is causing the error
+        ########################################################
     #     output = chain.run(input=user_input)
 
     #     st.session_state.past.append(user_input)
