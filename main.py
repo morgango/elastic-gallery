9c634fe503dbf976fd3a445d9a99e0be2b5138d8
morgango <morgango@gmail.com>
2023-07-21 15:56:55 -0500

    Commented and updated all functions

--- main.py
+++ main.py
@@ -41,78 +41,77 @@ handler.setFormatter(formatter)
 # add handler to logger
 logger.addHandler(handler)
 
-# # load the .env file and retrieve the things we need
-# load_dotenv(find_dotenv())
-# # NOTE: You will need to populate a file called .env in the same directory as main.py
-# # with key value pairs for the values below.  
-# OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-# ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
-# ELASTICSEARCH_INDEX = os.environ.get("ELASTICSEARCH_INDEX")
-
-# all the environment variables we care about
+# all the variables in the .env file that we care about
 env_list = ("elasticsearch_user", 
             "elasticsearch_pw", 
             "elasticsearch_host", 
             "elasticsearch_port", 
-            "elasticsearch_model", 
+            "elasticsearch_model_id", 
             "elasticsearch_cloud_id", 
             "elasticsearch_index", 
             "openai_api_key")
 
-# def check_env():
-#     if not load_dotenv(find_dotenv()):
-#         print("ERROR: load_dotenv returned error")
-#         return False
-
-#     if all(env in os.environ for env in env_list):
-#         print("ENV file found!")
-#         return True
-
-#     return False
-
-# def load_env(env_path=None):
-
-#     check_env()
-
-#     # NOTE: You will need to populate a file called .env in the same directory as main.py
-#     st.session_state['openai_api_key'] = os.environ.get('openai_api_key')
-#     st.session_state['elasticsearch_pw'] = os.environ.get('elasticsearch_pw')
-#     st.session_state['elasticsearch_user'] = os.environ.get('elasticsearch_user')
-#     st.session_state['elasticsearch_host'] = os.environ.get('elasticsearch_host')
-#     st.session_state['elasticsearch_port'] = os.environ.get('elasticsearch_port')
-#     st.session_state['elasticsearch_model_id'] = os.environ.get('elasticsearch_model_id')
-#     st.session_state['elasticsearch_cloud_id'] = os.environ.get('elasticsearch_cloud_id')
-#     st.session_state['elasticsearch_index'] = os.environ.get('elasticsearch_index')
-
-#     # build a few useful values
-#     st.session_state['elasticsearch_url'] = f"https://{st.session_state.elasticsearch_user}:{st.session_state.elasticsearch_pw}@{st.session_state.elasticsearch_host}:{st.session_state.elasticsearch_port}"
-#     os.environ['OPENAI_API_KEY'] = st.session_state.openai_api_key
-                                                        
 def write_temp_file(uploaded_file):
+    """
+    This function writes the contents of an uploaded file to a temporary file on the system, 
+    and then returns the path to that temporary file.
 
+    Parameters:
+    uploaded_file (io.BytesIO): The uploaded file as a BytesIO object.
+
+    Returns:
+    str: The path to the temporary file on the system.
+    """
+    # Create a new temporary file using the NamedTemporaryFile function from the tempfile module
+    # The delete=False parameter means that the file won't be deleted when it's closed
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        # Write the contents of the uploaded file to the temporary file
         tmp_file.write(uploaded_file.getvalue())
+        # Save the path to the temporary file
         tmp_file_path = tmp_file.name
+
+    # Return the path to the temporary file
     return tmp_file_path
 
 def extract_text_from_upload(uploaded_file, encoding="utf8", csv_loader_args={'delimiter': ','}):
-    
+    """
+    This function extracts text data from an uploaded file. The type of file - PDF, CSV, or text - 
+    is determined by checking the file's type. Appropriate loaders are used to extract the text
+    based on the file type.
+
+    Parameters:
+    uploaded_file (UploadedFile): The file uploaded by the user.
+    encoding (str): Optional. The encoding to be used for text or CSV files. Defaults to "utf8".
+    csv_loader_args (dict): Optional. Additional arguments to be passed to the CSV loader 
+                            (such as the delimiter). Defaults to {'delimiter': ','}.
+
+    Returns:
+    str: The extracted text data from the file.
+    """
+    # Write the uploaded file to a temporary file on the system
     tmp_file_path = write_temp_file(uploaded_file)
+
+    # Convert the file's type to lower case and remove leading/trailing whitespace
     file_type = uploaded_file.type.lower().strip()
+
+    # Check the file type and create the appropriate loader
     if "pdf" in file_type:
         loader = PyPDFLoader(file_path=tmp_file_path)
     elif "csv" in file_type:
         loader = CSVLoader(file_path=tmp_file_path, 
-                        encoding=encoding, 
-                        csv_args=csv_loader_args)
+                           encoding=encoding, 
+                           csv_args=csv_loader_args)
     elif "text" in file_type:
         loader = TextLoader(tmp_file_path)
     else:
+        # If the file type isn't recognized, assume it's a text file
         loader = TextLoader(tmp_file_path)
 
+    # Use the loader to extract the text data from the file
     text = loader.load()
-    return text
 
+    # Return the extracted text
+    return text
 
 def load_document_text(text, 
                        elasticsearch_url=None, 
@@ -122,8 +121,28 @@ def load_document_text(text,
                        chunk_size=1000, 
                        chunk_overlap=200, 
                        length_function=len):
+    """
+    This function splits a given text into chunks, and then uploads these chunks to an Elasticsearch 
+    cluster using an instance of the ElasticVectorSearch class.
 
-    # split into chunks
+    Parameters:
+    text (str): The text to be split and uploaded.
+    elasticsearch_url (str): Optional. The URL of the Elasticsearch cluster. Defaults to None.
+    index_name (str): Optional. The name of the Elasticsearch index where the chunks will be uploaded. Defaults to None.
+    embeddings (np.ndarray or list of np.ndarray): Optional. The embeddings for the chunks, if any. Defaults to None.
+    separator (str): Optional. The character used to split the text into chunks. Defaults to "\n".
+    chunk_size (int): Optional. The size of each chunk. Defaults to 1000.
+    chunk_overlap (int): Optional. The size of the overlap between consecutive chunks. Defaults to 200.
+    length_function (function): Optional. The function used to compute the length of a chunk. Defaults to len.
+
+    This function first creates an instance of the CharacterTextSplitter class, which splits the input text into 
+    chunks. Each chunk will be of size `chunk_size`, and consecutive chunks will overlap by `chunk_overlap` characters.
+    The chunks are split at the character specified by `separator`.
+
+    The chunks are then uploaded to the Elasticsearch cluster at the specified URL, and stored in the specified index.
+    If embeddings are provided, they will be associated with the chunks.
+    """
+    # Create an instance of CharacterTextSplitter
     text_splitter = CharacterTextSplitter(
         separator=separator,
         chunk_size=chunk_size,
@@ -131,57 +150,184 @@ def load_document_text(text,
         length_function=length_function,
     )
 
+    # Split the text into chunks
     chunks = text_splitter.split_documents(text)
     
-    # this will upload the chunks to the elasticsearch cluster
+    # Upload the chunks to the Elasticsearch cluster
     uploaded = ElasticVectorSearch.from_documents(chunks, embeddings,
-                                                elasticsearch_url=elasticsearch_url, 
-                                                index_name=index_name)
+                                                  elasticsearch_url=elasticsearch_url, 
+                                                  index_name=index_name)
 
 def ask_es_question(question, elasticsearch_url=None, index_name=None, embeddings=None):
-    
+    """
+    This function uses an instance of the ElasticVectorSearch class to perform a similarity search on a given 
+    Elasticsearch index. It takes a question as input and returns the documents that are most similar to the question.
+
+    Parameters:
+    question (str): The question to be used for the similarity search.
+    elasticsearch_url (str): Optional. The URL of the Elasticsearch cluster. Defaults to None.
+    index_name (str): Optional. The name of the Elasticsearch index where the search will be performed. Defaults to None.
+    embeddings (np.ndarray or list of np.ndarray): Optional. The embeddings for the question, if any. Defaults to None.
+
+    Returns:
+    list: A list of documents that are most similar to the input question.
+
+    The function first creates an instance of the ElasticVectorSearch class, which connects to the specified Elasticsearch
+    cluster and index, and sets up the specified embeddings.
+
+    The function then performs a similarity search on the index using the input question and returns the most similar documents.
+    """
+    # Create an instance of ElasticVectorSearch
     knowldedge_base = ElasticVectorSearch(elasticsearch_url=elasticsearch_url, 
-                                            index_name=index_name, 
-                                            embedding=embeddings)
+                                          index_name=index_name, 
+                                          embedding=embeddings)
+
+    # Perform a similarity search on the index using the input question
     docs = knowldedge_base.similarity_search(question)
 
+    # Return the most similar documents
     return docs
 
 def load_conv_chain(temperature=0.5, open_api_key=""):
-    """Logic for loading the chain you want to use should go here."""
+    """
+    This function creates and returns an instance of the ConversationChain class, which uses an instance 
+    of the OpenAI class to generate responses in a conversation.
+
+    Parameters:
+    temperature (float): Optional. The temperature parameter for the OpenAI API, which controls the randomness 
+                         of the generated responses. A higher value makes the responses more random, while 
+                         a lower value makes them more deterministic. Defaults to 0.5.
+    open_api_key (str): Optional. The API key for OpenAI. Defaults to "".
+
+    Returns:
+    ConversationChain: An instance of the ConversationChain class.
+
+    The function first creates an instance of the OpenAI class, using the provided temperature and API key. 
+    This OpenAI instance is responsible for generating responses in a conversation.
+
+    The function then creates an instance of the ConversationChain class, using the OpenAI instance, and returns it.
+    """
+    # Create an instance of OpenAI
     llm = OpenAI(temperature=temperature, openai_api_key=open_api_key)
+    
+    # Create an instance of ConversationChain
     chain = ConversationChain(llm=llm)
+
+    # Return the ConversationChain instance
     return chain
 
 def get_connections(provider="openai", **kwargs):
+    """
+    This function initializes and returns an embeddings provider and a language model 
+    based on the specified provider name.
 
-    cleaned_provider=provider.lower().strip()
+    Parameters:
+    provider (str): Optional. The name of the provider to use for embeddings and language modeling. Defaults to "openai".
+    kwargs (dict): Optional. Additional arguments that can be passed to the embeddings provider and language model.
+
+    Returns:
+    tuple: A tuple containing the embeddings provider and the language model.
+
+    The function checks the provider name, and based on this, it creates and returns an instance of the OpenAIEmbeddings 
+    class (for generating embeddings) and an instance of the OpenAI class (for language modeling).
+
+    Currently, it supports only the "openai" provider, and if any other provider name is passed, it will default to "openai".
+
+    Note: Additional parameters for the embeddings provider and language model can be passed via **kwargs, but they are not 
+    used in the current implementation.
+    """
+    # Clean the provider name
+    cleaned_provider = provider.lower().strip()
+
+    # Check the provider name and create the appropriate embeddings provider and language model
     if cleaned_provider == "openai":
         embeddings = OpenAIEmbeddings()
         llm = OpenAI()
     else:
+        # Default to "openai" if the provider name is not recognized
         embeddings = OpenAIEmbeddings()
         llm = OpenAI()
 
+    # Return the embeddings provider and the language model
     return embeddings, llm
 
 def load_env(env_path=None):
+    """
+    This function loads environment variables from a .env file using the python-dotenv library, 
+    and then returns a dictionary-like object containing the environment variables.
+
+    Parameters:
+    env_path (str): Optional. The path to the .env file. If not provided, the load_dotenv() function 
+                    will look for the .env file in the current directory and if not found, then 
+                    recursively up the directories. Defaults to None.
+
+    Returns:
+    os._Environ: This is a dictionary-like object which allows accessing environment variables.
+    """
+    # Load environment variables from a .env file
     load_dotenv(env_path)
+
+    # Return a dictionary-like object containing the environment variables
     return os.environ
 
-def load_session_vars(os_env_vars={}):
-    for key, value in os_env_vars.items():
-        logger.debug(f"{key} = {value}")
+def load_session_vars(os_env_vars={}, key_list=None):
+    """
+    This function loads variables into Streamlit's session state.
+
+    Parameters:
+    os_env_vars (dict): Optional. A dictionary of environment variables to load into the session state. 
+                        Defaults to an empty dictionary.
+    key_list (list): Optional. A list of keys. If provided, only environment variables 
+                     with these keys will be loaded into the session state. Defaults to None.
+
+    The function first checks if key_list is provided. If so, it creates a new dictionary
+    with key-value pairs where keys are elements in key_list and the values are corresponding
+    values from os.environ.
+    
+    If key_list is not provided, it uses the os_env_vars dictionary.
+    
+    It then logs the dictionary of variables that will be loaded into the session state.
+
+    Finally, it iterates over the dictionary of variables, logging each one, and adds each 
+    variable to the session state.
+    """
+    # Check if key_list is provided
+    if key_list is not None:
+        # If so, create a new dictionary with key-value pairs for each key in key_list
+        vars = {env: os.environ[env] for env in key_list if env in os.environ}
+    else:
+        # If not, use the os_env_vars dictionary
+        vars = os_env_vars
+
+    # Log the dictionary of variables
+    logger.debug(f"{vars}")
+
+    # Iterate over the dictionary of variables
+    for key, value in vars.items():
+        # Log each variable
+        logger.debug(f"Setting st.{key} = {value}")
+        # Add the variable to the session state
         st.session_state[key] = value
 
-env_file_loaded = 'env_file_loaded' in st.session_state
+    # build the elasticsearch_url for ease of use
+    st.session_state['elasticsearch_url'] = f"https://{st.session_state['elasticsearch_user']}:{st.session_state['elasticsearch_pw']}@{st.session_state['elasticsearch_host']}:{st.session_state['elasticsearch_port']}"
 
-if not env_file_loaded:
+# load the environment file if needed.
+if not 'env_file_loaded' in st.session_state:
+
+    # upload variables from the .env file as environment variables
     env_path = find_dotenv()
     env_vars = dict(load_env(env_path=env_path))
-    load_session_vars(env_vars)
-    for key in env_list:
-        logger.debug(f"{key}={st.session_state[key]}")
+
+    # load our values from the .env file into the session state.
+    # only add our values, not the entire environment.
+    load_session_vars(os_env_vars=env_vars, key_list=env_list)
+
+    # we set this as an environment variable because it has to be present
+    # in so many different places
+    os.environ['OPENAI_API_KEY'] = st.session_state.openai_api_key
+
+    # don't load this multiple times
     st.session_state['env_file_loaded'] = True
 
 chain = load_conv_chain(open_api_key=st.session_state.openai_api_key)
